a05cf2f2f715fb306c77678dbf65a2b664bd766f
githubofnan <260518836@qq.com>
2019-03-08 10:42:45 +0800

    first

---
+++ src/views/html/index/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <span>this is index</span>
+    <img alt="Vue logo" src="../../../assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const glob = require('glob')
+
+const PAGE_TITLE = {
+  index: '首页',
+  login: '登录',
+}
+
+const getPages = () => {
+  let pages = {};
+  const FILE_NAME = path.resolve(__dirname, './src/views');
+  let entryFile = glob.sync(FILE_NAME + '/**/**/main.js');
+  entryFile.forEach((item) => {
+    let pageInfo = {};
+    let pathName = item.split('/').splice(-5);
+    pageInfo['entry'] = pathName.join('/');
+    pageInfo['template'] = 'public/index.html',
+    pageInfo['title'] = PAGE_TITLE[pathName[3]] || '';
+    pageInfo['filename'] = pathName.slice(1, 4).join('/')+'.html';
+    pageInfo['chunks'] = ['chunk-vendors', 'chunk-common', pathName[3]];
+    pages[pathName[3]] = pageInfo;
+  })
+  console.log(pages)
+  return pages;
+}
+
+module.exports = {
+  pages: getPages()
+}
