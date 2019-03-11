29ae7baf71ffa1b7e4770e924c59ab18b2a28311
githubofnan <260518836@qq.com>
2019-03-11 10:02:04 +0800

    pilyfill

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   presets: [
-    [
-      '@vue/app',
-      {
-        useBuiltIns: 'entry'
-      }
-    ]
+    ['@vue/app', {
+      polyfills: [
+        'es6.promise',
+        'es6.symbol'
+      ]
+    }]
   ],
   plugins: [
     [
